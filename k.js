7b3a43ebc3f97da8b9d9471cf0c42aa6574926d2
daskl <aboubackrskl@gmail.com>
2021-10-12 11:56:47 +0200

    fin de checkpoint

---
+++ k.js
@@ -0,0 +1,10 @@
+function accum(s) {
+	let str=String
+    let letter=String
+    for (let i = 0; i < array.length; i++) {
+        letter=str.charAt(0).touppercase
+    }
+    return (letter)
+}
+
+console.log(accum('abba'))
